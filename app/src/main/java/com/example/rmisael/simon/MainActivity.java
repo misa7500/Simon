9fa2f9d8c0382deec3b5cf21e318c7dfc6e46cd7
misa7500 <misa7500@gmail.com>
2017-06-02 13:17:32 -0500

    Update MainActivity.java
    Añadi Cambios en lista

--- app/src/main/java/com/example/rmisael/simon/MainActivity.java
+++ app/src/main/java/com/example/rmisael/simon/MainActivity.java
@@ -12,19 +12,27 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton Azul,Rojo,Verde,Naranja,Blanco_A,Blanco_R,Blanco_v,Blanco_N;
-    Azul = (Button) findViewById(R.id.A)
+    Azul = (ImageButton) findViewById(R.id.A)
 
     List lista = new ArrayList();
     int ran;
+    boolean ban = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ran =(int) (Math.random()*4)+1;
-
-
+         
+            ran =(int) (Math.random()*4)+1;
+            lista.add(ran);
+            Esperando(lista);
 
 
     }
+public void Esperando(List listal){
+        List listal = new ArrayList();
+        
+    
+}
+
 }
